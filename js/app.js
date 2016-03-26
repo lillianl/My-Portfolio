8de7f06a2ceefd18ff5e7081102bd7c4dba6ecc1
Lillian Liu <lillinoodles@gmail.com>
2016-03-26 02:32:15 -0700

    adding echo and aimhigh to project pages

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 $(document).foundation();
+
+  console.log("reaady");
+
+ $('div.columns').mouseover(function() {
+   //use $(this) to apply animate to opacity and left on whichever <li> registered the event 
+    $(this).animate( { 
+    left:50,
+    opacity:1
+    });
+    });
+  
+   
+  $('div.columns').mouseout(function () {
+        //chain the stop method to stop the current animation from playing without writing an additional line of code
+        $(this).animate({
+            opacity: .25,
+            left:0
+        });
+
+    });
+
+  //close jQuery
+});
